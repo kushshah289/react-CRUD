255456177a9c3e53ec120d96e530dd197c58eb52
kush <ks4437@nyu.edu>
2019-06-18 16:51:29 -0400

    feature changes

---
+++ src/features/EventList/EventListItem.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import {Segment, Item, Icon, List, Button} from 'semantic-ui-react';
+import EventListAttendees from './EventListAttendees';
+
+class EventListItem extends Component {
+    render() {
+      const {event} = this.props;
+        return (
+                 <Segment.Group>
+                    <Segment>
+                      <Item.Group>
+                        <Item>
+                            <Item.Image size="tiny" circular src={event.hostPhotoURL} />
+                          <Item.Content>
+                            <Item.Header as="a">{event.title}</Item.Header>
+                            <Item.Description>
+                              Hosted by <a>{event.hostedBy}</a>
+                            </Item.Description>
+                          </Item.Content>
+                        </Item>
+                      </Item.Group>
+                    </Segment>
+                    <Segment>
+                      <span>
+                        <Icon name="clock" /> {event.data} |
+                        <Icon name="marker" /> {event.venue}
+                      </span>
+                    </Segment>
+                    <Segment secondary>
+                      <List horizontal>
+                        {event.attendees.map(attendee => (
+                          <EventListAttendees attendee={attendee} key={attendee.id}/>
+                        ))}
+                        
+                        
+                      </List>
+                    </Segment>
+                    <Segment clearing>
+                        <span>{event.description}</span>
+                      <Button as="a" color="teal" floated="right" content="View" />
+                    </Segment>
+                  </Segment.Group>
+        )
+    }
+}
+
+export default EventListItem;
